af0c44e415fae2605bb1836dd4abe03f8bb242d9
Davor Kikindjanin <davor.kikindjanin@gmail.com>
2026-08-07 18:40:53 +0200

    chore: plugin skeleton and gitignore

---
+++ catalogops.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: CatalogOps
+ * Description: Bulk operations for large WooCommerce catalogs.
+ * Version: 0.0.1
+ * Requires PHP: 8.1
+ * Author: 
+ */
+defined( 'ABSPATH' ) || exit;
